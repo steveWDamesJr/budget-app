5d0d791014044e254b33e555987bf4e9e6acfe2f
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-08-10 13:26:00 -0500

    Configure users_controller to track user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,34 +3,16 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @user = current_user
   end
 
   # GET /users/1 or /users/1.json
   def show
-    @user = current_user
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
+    @user = User.find(params[:id])
+    @groups = Group.where(user_id: params[:id])
   end
 
   # GET /users/1/edit
   def edit; end
-
-  # PATCH/PUT /users/1 or /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # DELETE /users/1 or /users/1.json
   def destroy
