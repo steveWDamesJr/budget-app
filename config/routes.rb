a494316a790969db8db67e547b06ad857b261907
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-08-10 13:41:05 -0500

    Update routes to include all page associations

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root to: "splashes#index"
-
-    resources :users
-    resources :groups, only: [ :new, :create, :show ] do
-      resources :purchases, only: [ :new, :create ]
+  
+  resources :users, only: [:index, :show] do
+    resources :groups, only: [ :new, :create, :show, :destroy ] do
+      resources :purchases, only: [ :new, :create, :destroy ]
     end
-
+  end
+  
+  root to: "splashes#index"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
