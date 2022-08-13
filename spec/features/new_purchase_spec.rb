4be7a8bbf1408558e30462e57dc678f8e63fda3d
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-08-13 08:31:26 -0500

    Update test for assigning purchase to type

--- spec/features/new_purchase_spec.rb
+++ spec/features/new_purchase_spec.rb
@@ -46,3 +46,44 @@ RSpec.describe 'Test New purchase page', type: :feature do
     end
   end
 end
+
+
+RSpec.describe 'Test New purchase page', type: :feature do
+  describe 'GET new purchase page' do
+    before(:each) do
+      visit user_session_path
+      @user = FactoryBot.create(:user)
+      sign_in @user
+
+      visit new_user_group_path(:user_id) do
+        select 'Clothing', from: 'icon'
+        fill_in 'Name', with: 'Pants'
+
+        click_button 'Add Purchase Type'
+        
+        click_button 'Add Purchase Type'
+
+        select 'Tech', from: 'icon'
+        fill_in 'Name', with: 'Laptop'
+
+        click_button 'Add Purchase Type'
+    end
+    it 'can add assign a new purchase to type' do
+        click_button 'Pants'
+
+        click_button 'Add new purchase'
+
+        fill_in 'Laptop'
+
+        fill_in '40.00'
+
+        select 'Tech', from: 'icon'
+
+        click_button 'Add Purchase'
+
+        expect(page).to have_content('Laptop')
+      end
+    end
+  end
+end
+    
